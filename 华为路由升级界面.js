41234cd3a21e0f45df53502f8db2427e7dbce3e2
fgfg163 <fgfg163@163.com>
2021-10-31 14:24:05 +0800

    Update 华为路由升级界面.js

--- 华为路由升级界面.js
+++ 华为路由升级界面.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         华为路由升级界面
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       fgfg163
 // @run-at       document-body
@@ -15,6 +15,15 @@
     'use strict';
     const s = document.createElement('style');
     s.type = 'text/css';
-    s.innerHTML = '#upgrade_detect_new_version,#index_autoUpg{display:none !important;}';
+    s.innerHTML = '#upgrade_detect_new_version,#index_autoUpg,body *{display:none !important;}';
     document.head.appendChild(s);
+
+    const div = document.createElement('div');
+    div.id = 'upgrade_redirect_ignor';
+    div.onclick = function() {
+        window.EMUI.upgradeRedirectIgnor.redirectIgnor();
+    };
+    div.style = 'cursor:pointer;display:block !important;width:100vw;height:100vh;text-align:center;line-height:50vh;';
+    div.innerText = '稍后提醒';
+    document.body.appendChild(div);
 })();
