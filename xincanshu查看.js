e40ede9c9c0ec1e00b6d53bb104833b88b0b4a1d
fgfg163 <fgfg163@163.com>
2022-01-20 14:45:05 +0800

    Update xincanshu查看.js

--- xincanshu查看.js
+++ xincanshu查看.js
@@ -1,23 +1,20 @@
 // ==UserScript==
 // @name         xincanshu查看
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       fgfg163
-// @match        https://www.xincanshu.com/cpu/**
+// @match        https://www.xincanshu.com/**
 // @icon         https://www.google.com/s2/favicons?domain=xincanshu.com
+// @run-at       document-body
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    var className1 = document.querySelector('[class^=denglutishi]').classList[0] || '';
-    var className2 = document.querySelector('[class^=zheceng]').classList[0] || '';
-
     var style = document.createElement('style');
     style.type = 'text/css';
     style.rel = 'stylesheet';
-    style.innerHTML = `.${className1},.${className2}{display: none !important;} #chart-wrapper{filter:none !important;}`
+    style.innerHTML = `[class^=denglutishi],[class^=zheceng]{display: none !important;} #chart-wrapper{filter:none !important;}`
     document.head.appendChild(style);
 })();
